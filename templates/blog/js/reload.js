4941762c23dc9e2bbebb8052b959ffa0e9f93451
taichi <ryushi@gmail.com>
2012-05-25 18:11:15 +0900

    update bootstrap.

--- templates/blog/js/reload.js
+++ templates/blog/js/reload.js
@@ -1,16 +1,16 @@
 if ("WebSocket" in window) {
 	var url = "ws://localhost:8080";
 	console.log("connect to " + url);
-	var ws = new WebSocket("url");
+	var ws = new WebSocket(url);
 	ws.onopen = function() {
-		console.log('connected...');
+		console.log("connected...");
 	};
 	ws.onmessage = function() {
 		console.log(event.data);
 		location.reload();
 	};
 	ws.onclose = function() {
-		console.log('closed');
+		console.log("closed");
 	};
 } else {
 	alert("Auto reloading is unsupported because WebSocket is unsupported.")
